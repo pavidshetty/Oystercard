4ab597b066d5f200542805cbab148c43da5689a0
pavidshetty <pavitrashetty23@gmail.com>
2019-11-06 10:27:40 +0000

    adding balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,11 @@ describe Oystercard do
     oystercard = Oystercard.new
     expect(oystercard).to be_instance_of(Oystercard)
   end
+
+
+it 'has 0 balance' do
+  oystercard = Oystercard.new
+  expect(oystercard.balance).to eq 0
+end
+
 end
